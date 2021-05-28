f61d2d33ac68988c0b227275d5e60b02b4eda482
kleber <kleberrocha2701@gmail.com>
2021-05-27 21:06:25 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const App:React.FC = () =>  {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bom dia</h1>
+      </header>
+    </div>
+  );
+}
+
+
